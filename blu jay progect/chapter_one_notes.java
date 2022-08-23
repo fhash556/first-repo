2d3fa669aad86a7415b438a191b43f56f0a6cbbd
fhash556 <fshashmi@stu.naperville203.org>
2022-08-23 12:34:09 -0500

    notes

---
+++ blu jay progect/chapter_one_notes.java
@@ -0,0 +1,42 @@
+
+/**
+ * faisal hashmi
+ * 8/23/22
+ * notes for ch1
+ * Write a description of class chapter_one_notes here.
+ *who helped me
+ * 
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class chapter_one_notes
+{
+    /**
+     * An example of a method - replace this comment with your own
+     *where we are goning to code
+     *
+     */
+    public static void main(String[] args)
+    {
+        
+        Scanner key = new Scanner(System.in);
+        
+        // these are the basic data type
+        int x = 9,y,z;
+        
+        double num = 5, num1 = 23.65456;
+        
+        char letter = 'd';
+        
+        boolean choice 
+        
+        // these are objects 
+        
+        String word = new String("faisal"); 
+        
+    }
+    
+}
